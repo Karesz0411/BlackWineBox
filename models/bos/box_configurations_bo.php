ce6e214d51f4e3bc5a516557349a5fcb9d8fc0ab
trissz <triszten25@gmail.com>
2024-03-09 12:06:45 +0100

    Update box_configurations_bo.php

--- models/bos/box_configurations_bo.php
+++ models/bos/box_configurations_bo.php
@@ -40,6 +40,31 @@
 		* ********************************************************
 		* ********************************************************/
 
+		public function getList() {
+			$do_factory = new DoFactory();
+			$do_list = [];
+			
+			$records = $this->dao->getList();
+			
+			if (empty($records)) {
+				UserMessagesHelper::addToMessages(
+					"Nincs még feltöltött box konfigurációk!",
+					UserMessagesHelper::MESSAGE_LEVEL_ERROR
+				);
+			}
+			else {
+				foreach ($records as $record) {
+					$do_list[] = $do_factory->get(DoFactory::BOX_CONFIGURATIONS, $record);
+				}
+			}
+			
+			return $do_list;
+		}
+
+		/* ********************************************************
+		* ********************************************************
+		* ********************************************************/
+
 		public function create(BoxConfigurationsDo $do) {
 			return ($this->dao)->create(
 				[
